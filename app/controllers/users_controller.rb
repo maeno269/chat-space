91bc568050c4229087a84ed19ee757ba622d03bf
maeno269 <good.luck.m.k.18@ezweb.ne.jp>
2018-09-28 14:19:18 +0900

    Chatspace-group-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,19 @@
 class UsersController < ApplicationController
 
-def edit
-end
-
-def update
-  if current_user.update(user_params)
-    rederect_to root_path
-  else
-    render :edit
+  def edit
   end
-end
 
-private
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
-def user_params
-  params.require(:user).permit(:name, :email)
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
